0750ddd9f92d1af48447a4e427322825aa6b7140
SirZif <sirzif6@gmail.com>
2026-07-01 13:27:19 +0300

    some changes idk

--- lua/tzfn/groups/telescope.lua
+++ lua/tzfn/groups/telescope.lua
@@ -1,12 +1,16 @@
-return function(palette, styles)
+return function(palette, _)
 	return {
 		TelescopeBorder = { fg = palette.cyn, bg = palette.surface },
-		TelescopeMatching = { fg = palette.blu2 },
 		TelescopeNormal = { link = "NormalFloat" },
-		TelescopePromptNormal = { link = "TelescopeNormal" },
-		TelescopePromptPrefix = { fg = palette.cyn2 },
-		TelescopeSelection = { fg = "NONE", bg = palette.overlay },
-		TelescopeSelectionCaret = { fg = palette.cyn2, bg = palette.overlay },
-		TelescopeTitle = { fg = palette.red, bold = styles.bold },
+		TelescopePromptBorder = { fg = palette.mgt2, bg = palette.surface },
+		TelescopePromptTitle = { fg = palette.mgt2, bg = palette.surface },
+		TelescopeResultsComment = { fg = palette.subtle },
+
+		-- TelescopeMatching = { fg = palette.fg },
+		-- TelescopePromptNormal = { link = "TelescopeNormal" },
+		-- TelescopePromptPrefix = { fg = palette.cyn2 },
+		-- TelescopeSelection = { fg = "NONE", bg = palette.overlay },
+		-- TelescopeSelectionCaret = { fg = palette.cyn2, bg = palette.overlay },
+		-- TelescopeTitle = { fg = palette.cyn, bold = styles.bold },
 	}
 end
